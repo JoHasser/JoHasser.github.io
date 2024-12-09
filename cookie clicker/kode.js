be8a8a8366e5463b162f3fa89bb180d283e09e47
johasser <johannes.sylta@gmail.com>
2024-12-09 15:30:34 +0100

    fikk lastet inn bilde og lagd en plan for hva jeg skal gjøre - fortsetter hjemme og de neste timene!

---
+++ cookie clicker/kode.js
@@ -0,0 +1,9 @@
+// Steg 1: Få et bilde av en cookie på nettsiden (i HTML og CSS)
+
+
+
+// Steg 2: Gjør det mulig å trykke på cookien (spill av lyd for hvert klikk)
+
+// Steg 3: Gi ett poeng for hvert klikk
+
+// Steg 4: Sett en begrensing på tid
