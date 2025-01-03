b1a61eeaaabced426fe1bc3f22dcd37b1b8dfcad
johasser <johannes.sylta@gmail.com>
2025-01-03 02:25:13 +0100

    a bunch of stuff
    ye

--- cookie clicker/kode.js
+++ cookie clicker/kode.js
@@ -1,9 +1,14 @@
-// Steg 1: Få et bilde av en cookie på nettsiden (i HTML og CSS)
-
-
+// Steg 1: Få et bilde av en cookie på nettsiden (i HTML og CSS) - Ferdig
 
 // Steg 2: Gjør det mulig å trykke på cookien (spill av lyd for hvert klikk)
 
+document.getElementById("bilde").addEventListener("click", klikk);
+function klikk() {
+    console.log("du klikket!")
+}
+
 // Steg 3: Gi ett poeng for hvert klikk
+
+let poeng
 
 // Steg 4: Sett en begrensing på tid
