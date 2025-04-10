d711dec0d0875398c505d6863882eb8e2f9cb6c4
johasser <johannes.sylta@gmail.com>
2025-04-10 15:38:36 +0200

    Har lagt til diskografien til musikken min!
    ye

--- cookie clicker/kode.js
+++ cookie clicker/kode.js
@@ -3,12 +3,20 @@
 // Steg 2: Gjør det mulig å trykke på cookien (spill av lyd for hvert klikk)
 
 document.getElementById("bilde").addEventListener("click", klikk);
-function klikk() {
-    console.log("du klikket!")
-}
 
 // Steg 3: Gi ett poeng for hvert klikk
 
-let poeng
+let poeng = 0;
 
+function oppdaterpoeng() {
+    document.getElementById("poeng-display").innerText = poeng;
+}
+
+function klikk() {
+    poeng = poeng + 1; // ++ funker også, men dette er mer oversiktlig.
+    console.log("Du klikket! " + poeng);
+    oppdaterpoeng();
+}
 // Steg 4: Sett en begrensing på tid
+
+setTimeout
